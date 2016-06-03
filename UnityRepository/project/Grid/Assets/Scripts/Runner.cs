2377d868e2ad0e662c42f945b8a672b89b09faa3
DimaHuts <dima_ec_2011@mail.ru>
2016-06-03 12:39:50 +0300

    Task 4

---
+++ UnityRepository/project/Grid/Assets/Scripts/Runner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    /// <summary>
+    /// класс который запускает игру
+    /// </summary>
+    class Runner : MonoBehaviour
+    {
+        void Start ()
+	    {
+            Grid.SetBackground();
+            Grid.SetSizeGrid(Grid.GetRows, Grid.GetColumns);
+            Grid.DrawGrid();
+            StartCoroutine(Grid.Complete());
+        }
+	
+	    void Update ()
+	    {
+	        StartCoroutine( Game.MonitoringChangeCells() );
+	    }
+    }
+}
